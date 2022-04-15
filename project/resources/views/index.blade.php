385e68aaa8b64858065125a424ddbb3e28c32435
Faizarabhi <faizarabhi8@gmail.com>
2022-04-15 14:40:32 +0000

    "update project"

--- project/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-@section('content')
-<h1> la vue </h1>
-
-@endsection
--- project/routes/web.php
+++ project/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\StockController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +16,3 @@ use App\Http\Controllers\StockController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-
-Route::get('/contact', 'StockController@contact');
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
